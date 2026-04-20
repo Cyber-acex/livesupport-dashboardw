4ff25ad75bc05be09a8962069a319b418593779d
Cyber Ace <cyberincognito15@gmail.com>
2026-04-20 08:03:13 +0100

    Add DATABASE_URL support for Railway deployment

--- db/database.js
+++ db/database.js
@@ -1,11 +1,26 @@
 ﻿const mysql = require("mysql2");
+const url = require("url");
 
-const db = mysql.createConnection({
+let config = {
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "livesupport"
-});
+    database: process.env.DB_NAME || "livesupport",
+    port: process.env.DB_PORT || 3306
+};
+
+if (process.env.DATABASE_URL) {
+    const dbUrl = url.parse(process.env.DATABASE_URL);
+    config = {
+        host: dbUrl.hostname,
+        user: dbUrl.auth.split(':')[0],
+        password: dbUrl.auth.split(':')[1],
+        database: dbUrl.pathname.slice(1), // remove leading /
+        port: dbUrl.port || 3306
+    };
+}
+
+const db = mysql.createConnection(config);
 
 function connectDatabase(callback) {
     db.connect(err => {
